cbba959af82839cfc04202fdf2d00af0a08ed782
MingJian Hong <hongmingjian@gmail.com>
2015-12-08 10:12:12 +0800

    加入版权信息

--- userapp/vga.h
+++ userapp/vga.h
@@ -1,5 +1,21 @@
 /**
  * vim: filetype=c:fenc=utf-8:ts=4:et:sw=4:sts=4
+ *
+ * Copyright (C) 2015 Hong MingJian<hongmingjian@gmail.com>
+ * All rights reserved.
+ *
+ * This file is part of the EPOS.
+ *
+ * Redistribution and use in source and binary forms are freely
+ * permitted provided that the above copyright notice and this
+ * paragraph and the following disclaimer are duplicated in all
+ * such forms.
+ *
+ * This software is provided "AS IS" and without any express or
+ * implied warranties, including, without limitation, the implied
+ * warranties of merchantability and fitness for a particular
+ * purpose.
+ *
  */
 #ifndef _VGA_H
 #define _VGA_H

885cba670dad1663f76be3f6436974305d6a6b22
MingJian Hong <hongmingjian@gmail.com>
2020-03-01 12:25:45 +0800

    Improvement: 加入__aeabi_idiv和__aeabi_idivmod

--- lib/softfloat.c
+++ lib/softfloat.c
@@ -49,3 +49,26 @@ unsigned int __aeabi_uidivmod(unsigned int num, unsigned int den)
 	__asm__ __volatile__ ("sub r1, %0, %1\n\t":  : "r" (num), "r" (den*quot) : "r1" );
 	return quot;
 }
+
+signed int __aeabi_idiv(signed int num, signed int den)
+{
+	signed int minus = 0;
+	signed int v;
+
+	if (num < 0)
+	{
+		num = -num;
+		minus = 1;
+	}
+	if (den < 0)
+	{
+		den = -den;
+		minus ^= 1;
+	}
+
+	v = __udivmodsi4(num, den, 0);
+	if (minus)
+		v = -v;
+
+	return v;
+}
